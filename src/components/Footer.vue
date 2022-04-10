0296dc90720fd2c5899301285f99aef1a180fd39
崔哲（家） <22708206@qq.com>
2022-04-11 00:23:14 +0800

    fix(Footer.vue): 修复导航栏已知错误
    修复导航栏已知错误

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="footer">
-        <van-tabbar v-model="active" active-color="#ff8200" @change="onChange" router>
+        <van-tabbar :value="active" active-color="#ff8200" router>
             <van-tabbar-item to="/home" icon="home-o">首页</van-tabbar-item>
             <van-tabbar-item to="/news" icon="friends-o" dot>消息</van-tabbar-item>
             <van-tabbar-item to="/user" icon="friends-o">个人中心</van-tabbar-item>
@@ -13,12 +13,20 @@ export default {
     name: 'Footer',
     data () {
         return {
-            active: 0
+
         }
     },
-    methods: {
-        onChange (i) {
-            this.active = i
+    computed: {
+        active () {
+            if (this.$route.name === 'home') {
+                return 0
+            } else if (this.$route.name === 'news') {
+                return 1
+            } if (this.$route.name === 'user') {
+                return 2
+            } else {
+                return 0
+            }
         }
     }
 }
